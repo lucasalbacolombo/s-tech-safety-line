9b764570f4afca650b71de8687263246f70e1ad1
Lucas Colombo <colombo.lucas@outlook.com>
2022-09-02 15:12:09 -0300

    new products added

--- src/components/ProtecaoVisual/index.tsx
+++ src/components/ProtecaoVisual/index.tsx
@@ -1,14 +1,14 @@
-import { Link } from 'react-router-dom';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import { Grid } from '@mui/material';
 import OculosRJ from '../../assets/images/rj1.jpg';
+import OculosRJFume from '../../assets/images/rj-fume.jpg';
 import OculosLeopardo from '../../assets/images/leopardo-incolor.jpg';
 import OculosSobrepor from '../../assets/images/sobrepor.jpg';
+import SobreporFume from '../../assets/images/sobrepor-fume.png';
+import LeopardoFume from '../../assets/images/leopardo-cinza.jpg';
 
 export function ProtecaoVisual() {
   return (
@@ -23,21 +23,31 @@ export function ProtecaoVisual() {
           />
           <CardContent>
             <Typography gutterBottom variant='h5' component='div'>
-              Óculos de Proteção Modelo RJ
+              Óculos de Proteção Modelo RJ Incolor
             </Typography>
             <Typography variant='body2' color='text.secondary'>
-              <p>Incolor ou fumê</p>
               <p>CA válido até 2024</p>
             </Typography>
           </CardContent>
-          <CardActions>
-            <Link
-              to={'/produtos/protecao-visual'}
-              style={{ textDecoration: 'none', color: 'white' }}
-            >
-              <Button size='small'>Saiba Mais</Button>
-            </Link>
-          </CardActions>
+        </Card>
+      </Grid>
+
+      <Grid item xs={11} sm={6} md={3}>
+        <Card sx={{ maxWidth: 345 }}>
+          <CardMedia
+            component='img'
+            height='200'
+            image={OculosRJFume}
+            alt='oculos modelo rio de janeiro'
+          />
+          <CardContent>
+            <Typography gutterBottom variant='h5' component='div'>
+              Óculos de Proteção Modelo RJ Fumê
+            </Typography>
+            <Typography variant='body2' color='text.secondary'>
+              <p>CA válido até 2024</p>
+            </Typography>
+          </CardContent>
         </Card>
       </Grid>
 
@@ -51,21 +61,31 @@ export function ProtecaoVisual() {
           />
           <CardContent>
             <Typography gutterBottom variant='h5' component='div'>
-              Óculos de Proteção Modelo Sobrepor
+              Óculos de Proteção Modelo Sobrepor Incolor
             </Typography>
             <Typography variant='body2' color='text.secondary'>
-              <p>Incolor ou fumê</p>
               <p>CA válido até 2024</p>
             </Typography>
           </CardContent>
-          <CardActions>
-            <Link
-              to={'/produtos/protecao-visual'}
-              style={{ textDecoration: 'none', color: 'white' }}
-            >
-              <Button size='small'>Saiba Mais</Button>
-            </Link>
-          </CardActions>
+        </Card>
+      </Grid>
+
+      <Grid item xs={11} sm={6} md={3}>
+        <Card sx={{ maxWidth: 345 }}>
+          <CardMedia
+            component='img'
+            height='200'
+            image={SobreporFume}
+            alt='oculos modelo sobrepor'
+          />
+          <CardContent>
+            <Typography gutterBottom variant='h5' component='div'>
+              Óculos de Proteção Modelo Sobrepor Fumê
+            </Typography>
+            <Typography variant='body2' color='text.secondary'>
+              <p>CA válido até 2024</p>
+            </Typography>
+          </CardContent>
         </Card>
       </Grid>
 
@@ -79,21 +99,31 @@ export function ProtecaoVisual() {
           />
           <CardContent>
             <Typography gutterBottom variant='h5' component='div'>
-              Óculos de Proteção Modelo Leopardo
+              Óculos de Proteção Modelo Leopardo Incolor
             </Typography>
             <Typography variant='body2' color='text.secondary'>
-              <p>Incolor ou fumê</p>
               <p>CA válido até 2024</p>
             </Typography>
           </CardContent>
-          <CardActions>
-            <Link
-              to={'/produtos/protecao-visual'}
-              style={{ textDecoration: 'none', color: 'white' }}
-            >
-              <Button size='small'>Saiba Mais</Button>
-            </Link>
-          </CardActions>
+        </Card>
+      </Grid>
+
+      <Grid item xs={11} sm={6} md={3}>
+        <Card sx={{ maxWidth: 345 }}>
+          <CardMedia
+            component='img'
+            height='200'
+            image={LeopardoFume}
+            alt='oculos modelo leopardo'
+          />
+          <CardContent>
+            <Typography gutterBottom variant='h5' component='div'>
+              Óculos de Proteção Modelo Leopardo Fumê
+            </Typography>
+            <Typography variant='body2' color='text.secondary'>
+              <p>CA válido até 2024</p>
+            </Typography>
+          </CardContent>
         </Card>
       </Grid>
     </>
