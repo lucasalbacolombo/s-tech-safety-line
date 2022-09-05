cbe3aa1fbac22acb9d0393c1de2c5deb3ad0df61
Lucas Colombo <colombo.lucas@outlook.com>
2022-09-05 16:38:44 -0300

    about-us page

--- src/pages/Contato/index.tsx
+++ src/pages/Contato/index.tsx
@@ -3,6 +3,7 @@ import style from './style.module.scss';
 import ContactUs from '../../assets/images/contact-us.jpg';
 import PhoneIcon from '@mui/icons-material/Phone';
 import EmailIcon from '@mui/icons-material/Email';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
 
 export function Contato() {
   return (
@@ -14,13 +15,20 @@ export function Contato() {
       </div>
       <div className={style.contact}>
         <div className={style.phone}>
-          <PhoneIcon sx={{ fontSize: '60px' }} />
-          <p>Telefone: (11) 95610-2777</p>
+          <div>
+            <PhoneIcon sx={{ fontSize: '50px' }} />
+            <WhatsAppIcon sx={{ fontSize: '50px', ml: '30px' }} />
+          </div>
+          <p>
+            Entre em contato com nosso departamento de vendas pelo telefone ou
+            whatsapp.
+          </p>
+          <p>(11) 95610-2777</p>
         </div>
         <div className={style.phone}>
-          <EmailIcon sx={{ fontSize: '60px' }} />
+          <EmailIcon sx={{ fontSize: '50px' }} />
+          <p>Entre em contato por e-mail</p>
           <p>
-            E-mail:{' '}
             <a href='mailto:fabioepis@hotmai.com'>fabioepis@hotmail.com</a>
           </p>
         </div>
--- src/pages/SobreNos/index.tsx
+++ src/pages/SobreNos/index.tsx
@@ -1,3 +1,52 @@
+import style from './style.module.scss';
+import { Navbar } from '../../components/Navbar';
+import Image from '../../assets/images/about-us-image.jpeg';
+import { Footer } from '../../components/Footer';
+import Button from '@mui/material/Button';
+import { Link } from 'react-router-dom';
+
 export function SobreNos() {
-  return <></>;
+  return (
+    <>
+      <Navbar />
+      <div className={style.main} style={{ backgroundImage: `url(${Image})` }}>
+        <h2 className={style.text}>Sobre Nós</h2>
+      </div>
+      <div className={style.information1}>
+        <article>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel quam nisi
+          error ea repellendus! Eius modi, facilis deserunt eum doloremque porro
+          quas perspiciatis et molestiae sapiente voluptates id unde numquam.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel quam nisi
+          error ea repellendus! Eius modi, facilis deserunt eum doloremque porro
+          quas perspiciatis et molestiae sapiente voluptates id unde numquam.
+        </article>
+      </div>
+      <div className={style.buttons}>
+        <Link
+          to={'/produtos'}
+          style={{ textDecoration: 'none', color: 'white' }}
+        >
+          <Button
+            sx={{ backgroundColor: 'rgb(16, 28, 88)', m: '5px' }}
+            variant='contained'
+          >
+            Nossos Produtos
+          </Button>
+        </Link>
+        <Link
+          to={'/contato'}
+          style={{ textDecoration: 'none', color: 'white' }}
+        >
+          <Button
+            sx={{ backgroundColor: 'rgb(16, 28, 88)', m: '5px' }}
+            variant='contained'
+          >
+            Entre em contato conosco
+          </Button>
+        </Link>
+      </div>
+      <Footer />
+    </>
+  );
 }
